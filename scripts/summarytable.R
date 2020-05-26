385c2b0f37bdf1c8fbd06843a66056a6209c8826
rheaguliani2 <rheagul2@uw.edu>
2020-05-26 13:19:54 -0700

    fix table

--- scripts/summarytable.R
+++ scripts/summarytable.R
@@ -4,22 +4,31 @@ library("dplyr")
 library("knitr")
 
 #Load data frame
-tourists_vs_gdp <- read.csv("../data/tourists-vs-gdp.csv")
+tourists_vs_gdp <- read.csv("../data/tourists-vs-gdp.csv",
+                            stringsAsFactors = FALSE)
 
 #Summarize data by years 1995-2016
 world_tourists_vs_gdp <- tourists_vs_gdp %>%
   group_by(Year) %>%
   summarize(
-    total_GDP_per_capita = sum(GDP.per.capita, na.rm = TRUE),
-    total_tourists = sum(Tourists, na.rm = TRUE)
+    avg_GDP_per_capita = mean(GDP.per.capita, na.rm = TRUE),
+    avg_tourists = mean(Tourists, na.rm = TRUE)
   ) %>%
   slice(214:235)
+
+world_tourists_vs_gdp <- tourists_vs_gdp %>%
+  filter(Entity == "World") %>%
+  slice(6:27) %>%
+  select(-Entity) %>%
+  select(-Code) %>%
+  select(-X)
+  
 
 #Create table
 table_caption <- paste("Number of tourists outbound vs level of",
                        "prosperity of the world, 1995 to 2016")
 world_table <- kable(world_tourists_vs_gdp,
-                     col.names = c("Year", "GDP per Capita",
+                     col.names = c("Year", "Average GDP per Capita",
                                    "Number of Tourists"),
                      align = "lcr",
                      caption = table_caption)
