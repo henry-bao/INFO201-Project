278c9d6221e34f4cfd403003b55c651878d91e21
ERICKA <ericka08@uw.edu>
2020-05-26 16:11:50 -0700

    finalize charts 1 & 2

--- scripts/chart-1.R
+++ scripts/chart-1.R
@@ -1,5 +1,6 @@
 #Chart 1
-## This chart attempts to understand the relationship between 
+## 
+
 #Load libraries
 library("dplyr")
 library("ggplot2")
@@ -9,17 +10,17 @@ library("plotly")
 tourists_vs_gdp <- read.csv("../data/tourists-vs-gdp.csv",
                             stringsAsFactors = FALSE)
 
-#Narrowing data down to "World" data 1995-2016
-world_tourists_vs_gdp <- tourists_vs_gdp %>%
-  filter(Entity == "World") %>%
-  slice(6:27) %>%
+#Narrowing data extracted to data from 1995-2016
+tourists_vs_gdp_1995_2016 <- tourists_vs_gdp %>%
+  filter(Year == "1995" to "2016")
+slice(6:27) %>%
   select(-Entity) %>%
   select(-Code) %>%
   select(-X)
 
 #Creating plot 
 chart_1 <- ggplot(data = world_tourists_vs_gdp) +
-  geom_point(mapping = aes(x = Year, y = GDP.per.capita,
+  geom_point(mapping = aes(x = GDP.per.capita, y = Tourists,
                            color = Tourists)) +
   labs(title = "Average GDP per capita in the World per Year",
        color = "Tourists")
--- scripts/chart-3.R
+++ scripts/chart-3.R
@@ -1,10 +1,16 @@
 #Chart 3
+## This chart attempts to understand the relationship between 
+## the total number of tourists in the world per year and
+## the average GDP per Capita. In this chart, we are able to
+## understand that there is a huge increase in both number of
+## tourists and average GDP per Capita in the world. 
+
 #Load libraries
 library("dplyr")
 library("ggplot2")
 
 #Load data
-tourists_vs_gdp <- read.csv("./data/tourists-vs-gdp.csv",
+tourists_vs_gdp <- read.csv("../data/tourists-vs-gdp.csv",
                             stringsAsFactors = FALSE)
 
 #Narrowing data down to "World" data 1995-2016
@@ -15,9 +21,11 @@ world_tourists_vs_gdp <- tourists_vs_gdp %>%
   select(-Code) %>%
   select(-X)
 
-# Create plot
+#Creating plot 
 chart_3 <- ggplot(data = world_tourists_vs_gdp) +
   geom_col(mapping = aes(x = Year, y = Tourists,
-                           fill = GDP.per.capita)) +
-  labs(title = "World Tourism and GDP by Year",
+                         fill = GDP.per.capita)) +
+  labs(title = "Total Number of Tourists in the World per Year",
        fill = "Average GDP per Capita ($)")
+
+chart_3
