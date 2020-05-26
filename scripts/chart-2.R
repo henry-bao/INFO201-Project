9e0fcfced1a5e5cc970a173980fe457e8bedcc45
rheaguliani2 <rheagul2@uw.edu>
2020-05-26 15:11:00 -0700

    fix map

--- scripts/chart-2.R
+++ scripts/chart-2.R
@@ -1,7 +1,7 @@
 library(dplyr)
 library(leaflet)
 library(htmltools)
-
+library(plotly)
 df <- read.csv("../data/tourists-vs-gdp.csv", stringsAsFactors = FALSE)
 
 # Interactive Map
@@ -22,3 +22,38 @@ interactive_map <- leaflet(data = df) %>%
     label = ~lapply(info, HTML))
 
 interactive_map
+
+
+
+#plotly map
+#narrow data frame to 2016
+df_2016 <- df %>%
+  filter(Year == "2016")
+
+# light grey boundaries
+l <- list(color = toRGB("grey"), width = 0.5)
+
+# specify map projection/options
+g <- list(
+  showframe = FALSE,
+  showcoastlines = FALSE,
+  projection = list(type = 'Mercator')
+)
+
+#hover text
+hover_text <- paste("Country:", df_2016$Entity, '<br>',
+                    "Tourists:", df_2016$Tourists)
+
+#map
+chloropleth_map <- plot_geo(df_2016) %>%
+  add_trace(
+    z = ~GDP.per.capita, color = ~GDP.per.capita, colors = 'Blues',
+    text = hover_text, locations = ~Code, marker = list(line = l)
+    ) %>%
+  colorbar(title = 'GDP per capita', tickprefix = '$') %>%
+  layout(title = '2016 Global GDP & Number of Tourists',
+         geo = g)
+
+chloropleth_map
+
+
