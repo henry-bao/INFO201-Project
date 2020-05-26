8cf729dff38799a26b5ff4a569992abbd028bb78
ERICKA <ericka08@uw.edu>
2020-05-26 14:38:39 -0700

    work on chart 1

--- scripts/chart-1.R
+++ scripts/chart-1.R
@@ -1,12 +1,26 @@
-library(dplyr)
-library(ggplot2)
-library(htmltools)
-library(plotly)
+#Chart 1
 
-df <- read.csv("../data/tourists-vs-gdp.csv", stringsAsFactors = FALSE)
+#Load libraries
+library("dplyr")
+library("ggplot2")
 
-# Plot
+#Load data
+tourists_vs_gdp <- read.csv("../data/tourists-vs-gdp.csv",
+                            stringsAsFactors = FALSE)
 
-plot <- ggplot(df) +
-  geom_point(mapping = aes(x = "Tourists", y = "Year", color = "Entity"))
+#Narrowing data down to "World" data 1995-2016
+world_tourists_vs_gdp <- tourists_vs_gdp %>%
+  filter(Entity == "World") %>%
+  slice(6:27) %>%
+  select(-Entity) %>%
+  select(-Code) %>%
+  select(-X)
 
+#Creating plot 
+chart_1 <- ggplot(data = world_tourists_vs_gdp) +
+  geom_point(mapping = aes(x = Year, y = Tourists,
+                           color = GDP.per.capita)) +
+  labs(title = "Total Number of Tourists in the World per Year",
+       color = "Average GDP per Capita ($)")
+
+chart_1
