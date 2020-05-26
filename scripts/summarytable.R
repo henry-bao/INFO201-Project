c317fc87a195f91776a7af567ece30d26e631bc8
rheaguliani2 <rheagul2@uw.edu>
2020-05-26 12:18:20 -0700

    add table

---
+++ scripts/summarytable.R
@@ -0,0 +1,27 @@
+#Summary Table
+#Load libraries
+library("dplyr")
+library("knitr")
+
+#Load data frame
+tourists_vs_gdp <- read.csv("../data/tourists-vs-gdp.csv")
+
+#Summarize data by years 1995-2016
+world_tourists_vs_gdp <- tourists_vs_gdp %>%
+  group_by(Year) %>%
+  summarize(
+    total_GDP_per_capita = sum(GDP.per.capita, na.rm = TRUE),
+    total_tourists = sum(Tourists, na.rm = TRUE)
+  ) %>%
+  slice(214:235)
+
+#Create table
+table_caption <- paste("Number of tourists outbound vs level of",
+                       "prosperity of the world, 1995 to 2016")
+world_table <- kable(world_tourists_vs_gdp,
+                     col.names = c("Year", "GDP per Capita",
+                                   "Number of Tourists"),
+                     align = "lccr",
+                     caption = table_caption)
+  
+
