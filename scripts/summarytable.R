6396a397e147aa311227aedea334f464219552d5
rheaguliani2 <rheagul2@uw.edu>
2020-05-26 17:11:27 -0700

    turn summary table into a function

--- scripts/summarytable.R
+++ scripts/summarytable.R
@@ -8,23 +8,30 @@ library("knitr")
 tourists_vs_gdp <- read.csv("./data/tourists-vs-gdp.csv",
                             stringsAsFactors = FALSE)
 
-#Narrow data frame to just "world" data 1995 to 2016
-world_tourists_vs_gdp <- tourists_vs_gdp %>%
-  filter(Entity == "World") %>%
-  slice(6:27) %>%
-  select(-Entity) %>%
-  select(-Code) %>%
-  select(-X) %>%
-  round(digits = 2)
-
-#Create table using kable
-#table caption
-table_caption <- paste("Number of tourists outbound vs level of",
+#Function that returns dataset 
+get_summary_table <- function(df) {
+  #Narrow data frame to just "world" data 1995 to 2016
+  #There is no group-by function because the dataset already groups
+  #all the countries information into an Entity called "World" in
+  #the data frame. Filtering to "World" is the equivalent of grouping
+  #by year in this case.
+  world_tourists_vs_gdp <- df %>%
+    filter(Entity == "World") %>%
+    slice(6:27) %>%
+    select(-Entity) %>%
+    select(-Code) %>%
+    select(-X) %>%
+    round(digits = 2)
+  
+  #table caption
+  table_caption <- paste("Number of tourists outbound vs level of",
                        "prosperity of the world, 1995 to 2016")
-
-#create table
-world_table <- kable(world_tourists_vs_gdp,
+  #create table using kable
+  world_table <- kable(world_tourists_vs_gdp,
                      col.names = c("Year", "Average GDP per Capita",
                                    "Number of Tourists"),
                      align = "lcr",
                      caption = table_caption)
+  
+  world_table
+}
