e0c32830cd57219a5eccd2baedc265d6c7864a8b
ERICKA <ericka08@uw.edu>
2020-05-26 17:43:57 -0700

    make function

--- scripts/chart-1.R
+++ scripts/chart-1.R
@@ -12,24 +12,27 @@ library("plotly")
 tourists_vs_gdp <- read.csv("../data/tourists-vs-gdp.csv",
                             stringsAsFactors = FALSE)
 
-#Narrowing data extracted to data from 1995-2016
-tourists_vs_gdp_2000_2016 <- tourists_vs_gdp %>%
-  filter(Year =="2000" | Year == "2001" | Year == "2002" |Year == "2003" | 
-        Year == "2004" | Year == "2005" | Year == "2006" |Year == "2007" | 
-        Year == "2008" | Year == "2009" | Year == "2010" |Year == "2011" | 
-        Year == "2012" | Year == "2013" | Year == "2014" |Year == "2015" | 
-        Year == "2016")
-
-#Creating plot 
-chart_1 <- ggplot(data = tourists_vs_gdp_2000_2016) +
-  geom_point(mapping = aes(x = GDP.per.capita, y = Tourists,
-                           color = Year, text = paste("Entity:", Entity))
-             ) +
-  labs(
-    title = "Number of tourists outbound vs level of
-    prosperity of the home country, 2000 to 2016",
-    x = "GDP per capita", color = "Year")
-
-#Making the plot interactive
-ggplotly(chart_1)
-
+# Creat plot function
+get_plot <- function(df) {
+  
+  #Narrowing data extracted to data from 1995-2016
+  tourists_vs_gdp_2000_2016 <- df %>%
+    filter(Year =="2000" | Year == "2001" | Year == "2002" |Year == "2003" | 
+          Year == "2004" | Year == "2005" | Year == "2006" |Year == "2007" | 
+          Year == "2008" | Year == "2009" | Year == "2010" |Year == "2011" | 
+          Year == "2012" | Year == "2013" | Year == "2014" |Year == "2015" | 
+          Year == "2016")
+  
+  #Creating plot 
+  chart_1 <- ggplot(data = tourists_vs_gdp_2000_2016) +
+    geom_point(mapping = aes(x = GDP.per.capita, y = Tourists,
+                             color = Year, text = paste("Entity:", Entity))
+               ) +
+    labs(
+      title = "Number of tourists outbound vs level of
+      prosperity of the home country, 2000 to 2016",
+      x = "GDP per capita", color = "Year")
+  
+  #Making the plot interactive
+  ggplotly(chart_1)
+}
