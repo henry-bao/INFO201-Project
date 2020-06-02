f5333739e2b29d0ddbe017a9eb1773dbe88cfd76
kgraylin <62913743+kgraylin@users.noreply.github.com>
2020-06-01 22:46:30 -0700

    Update ui.R

--- finalapp/ui.R
+++ finalapp/ui.R
@@ -1,33 +1,79 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
+# ui.R
 library(shiny)
+library(plotly)
 
-# Define UI for application that draws a histogram
-shinyUI(fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot")
-        )
+# Define a variable `map_sidebar_content` that is a `sidebarPanel()` for your
+# first (map) page that contains:
+map_sidebar_content <- sidebarPanel(
+  # An input to select variable to map
+  selectInput(
+    "mapvar",
+    label = "Variable to Map",
+    choices = list(
+      "Population" = "population",
+      "Electoral Votes" = "votes",
+      "Votes / Population" = "ratio"
     )
-))
+  )
+)
+
+# Define a variable `map_main_content` that is a `mainPanel()` for your
+# first (map) page that contains the `plotlyOutput()` of your map
+map_main_content <- mainPanel(
+  plotlyOutput("map")
+)
+
+# Create a variable `map_panel` that stores a `tabPanel()` for your first page
+# It should include the following:
+map_panel <- tabPanel(
+  "Map",
+  titlePanel("Electoral College Votes"),
+  # A `sidebarLayout()` that contains...
+  sidebarLayout(
+    # Your `map_sidebar_content`
+    map_sidebar_content,
+    
+    # Your `map_main_content`
+    map_main_content
+  )
+)
+
+# Define a variable `scatter_sidebar_content` that is a `sidebarPanel()` for 
+# your second (scatter) page that contains:
+# - a `textInput()` widget for searching for a state in your scatter plot
+scatter_sidebar_content <- sidebarPanel(
+  textInput("search", label = "Find a State", value = "")
+)
+
+# Define a variable `scatter_main_content` that is a `mainPanel()` for your
+# second (scatter) page that contains the `plotlyOutput()` of your scatter
+scatter_main_content <- mainPanel(
+  plotlyOutput("scatter")
+)
+
+# Create a variable `scatter_panel` that stores a `tabPanel()` for your 2nd page
+# It should include the following:
+scatter_panel <- tabPanel(
+  "Scatter",
+  
+  # Add a titlePanel to your tab
+  titlePanel("Population v.s. Vote Power"),
+  
+  # Create a sidebar layout for this tab (page)
+  sidebarLayout(
+    
+    # Display your `scatter_sidebar_content`
+    scatter_sidebar_content,
+    
+    # Display your `scatter_main_content`
+    scatter_main_content
+  )
+)
+
+# Define a variable `ui` storing a `navbarPage()` element containing
+# your two pages defined above
+ui <- navbarPage(
+  "Electoral College",
+  map_panel, 
+  scatter_panel
+)
