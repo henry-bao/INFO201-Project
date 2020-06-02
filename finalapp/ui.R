16f58fa8f681b9cbeb53ba01e80856acffed5cf6
Henry Bao <henrybao@uw.edu>
2020-06-02 16:13:21 -0400

    added about page

--- finalapp/ui.R
+++ finalapp/ui.R
@@ -1,79 +1,52 @@
 # ui.R
-library(shiny)
-library(plotly)
 
-# Define a variable `map_sidebar_content` that is a `sidebarPanel()` for your
-# first (map) page that contains:
-map_sidebar_content <- sidebarPanel(
-  # An input to select variable to map
-  selectInput(
-    "mapvar",
-    label = "Variable to Map",
-    choices = list(
-      "Population" = "population",
-      "Electoral Votes" = "votes",
-      "Votes / Population" = "ratio"
+# Load libraries
+library("shiny")
+library("dplyr")
+library("ggplot2")
+library("plotly")
+
+# About page
+about <- tabPanel(
+  "About",
+  sidebarLayout(
+    sidebarPanel(
+      h2("About the data"),
+      tags$a(hrep = "https://ourworldindata.org/grapher
+      /number-of-tourists-outbound-vs-level-of-prosperity
+      -of-the-home-country","Number of Outbound Tourists
+      vs GDP Data-Set"),
+      p("The
+        data was collected through several avenues. The
+        country's prosperity was calculated based on the
+        Global World Bank's World Development Indicators
+        -- gross domestic product purchasing power parity
+        (GDP PPP). This is a country's GDP converted to
+        international dollars. The number of outbound tourists
+        was calculated based on data from the Global World Bank
+        which measured the number of times a citizen made a
+        departure from their country for non-commercial purposes.
+        This data set covers from 1995 to 2016.")
+    ),
+    mainPanel(
+      tags$img(src = "tourism.png"),
+      h2("Why Tourism ?"),
+      p("Tourism is a popular economic activity that has
+        developed significantly over the years. It's an
+        activity that can bring benefits to a country's
+        government and its local residents. Such activity
+        is recognized in almost every nation in the world.
+        Thus, it is important for us to know how can the wealth
+        of a country can impact tourism. Accordingly, for this
+        group project our team is going to analyze the correlation
+        between a country's GDP and the number of each country's 
+        outbound tourists.")
     )
   )
 )
 
-# Define a variable `map_main_content` that is a `mainPanel()` for your
-# first (map) page that contains the `plotlyOutput()` of your map
-map_main_content <- mainPanel(
-  plotlyOutput("map")
-)
 
-# Create a variable `map_panel` that stores a `tabPanel()` for your first page
-# It should include the following:
-map_panel <- tabPanel(
-  "Map",
-  titlePanel("Electoral College Votes"),
-  # A `sidebarLayout()` that contains...
-  sidebarLayout(
-    # Your `map_sidebar_content`
-    map_sidebar_content,
-    
-    # Your `map_main_content`
-    map_main_content
-  )
-)
-
-# Define a variable `scatter_sidebar_content` that is a `sidebarPanel()` for 
-# your second (scatter) page that contains:
-# - a `textInput()` widget for searching for a state in your scatter plot
-scatter_sidebar_content <- sidebarPanel(
-  textInput("search", label = "Find a State", value = "")
-)
-
-# Define a variable `scatter_main_content` that is a `mainPanel()` for your
-# second (scatter) page that contains the `plotlyOutput()` of your scatter
-scatter_main_content <- mainPanel(
-  plotlyOutput("scatter")
-)
-
-# Create a variable `scatter_panel` that stores a `tabPanel()` for your 2nd page
-# It should include the following:
-scatter_panel <- tabPanel(
-  "Scatter",
-  
-  # Add a titlePanel to your tab
-  titlePanel("Population v.s. Vote Power"),
-  
-  # Create a sidebar layout for this tab (page)
-  sidebarLayout(
-    
-    # Display your `scatter_sidebar_content`
-    scatter_sidebar_content,
-    
-    # Display your `scatter_main_content`
-    scatter_main_content
-  )
-)
-
-# Define a variable `ui` storing a `navbarPage()` element containing
-# your two pages defined above
 ui <- navbarPage(
-  "Electoral College",
-  map_panel, 
-  scatter_panel
+  "Tourism vs GDP",
+  about
 )
