bdfe3265e65556f28e250e24bb9eda553ed28b85
Henry Bao <henrybao@uw.edu>
2020-06-02 19:23:48 -0400

    edited color code to chart 1

--- scripts/chart-1.R
+++ scripts/chart-1.R
@@ -29,7 +29,7 @@ get_plot <- function(df) {
     labs(
       x = "GDP per capita", y = "Tourists Outbound", color = "Year") +
     scale_y_continuous(labels = function(x) format(x, scientific = FALSE)) +
-    scale_color_gradient(high = "red", low = "tan")
+    scale_color_gradient(high = "red", low = "#F9E79F")
 
   #Making the plot interactive
   ggplotly(chart_1)
