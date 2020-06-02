fc804bed1d355b4583f8b25445c3acd7e5e1b7f5
kgraylin <62913743+kgraylin@users.noreply.github.com>
2020-06-01 22:44:08 -0700

    Update app starter code

--- finalapp/app.R
+++ finalapp/app.R
@@ -7,43 +7,16 @@
 #    http://shiny.rstudio.com/
 #
 
-library(shiny)
+# Load libraries so they are available
+library("shiny")
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
+# Use source() to execute the `app_ui.R` and `app_server.R` files. These will
+# define the UI value and server function respectively.
+source("ui.R")
+source("server.R")
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
+# You will need to fill in the `app_ui.R` file to create the layout.
+# Run the app through this file.
 
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
-)
-
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
-}
-
-# Run the application 
+# Create a new `shinyApp()` using the loaded `ui` and `server` variables
 shinyApp(ui = ui, server = server)
