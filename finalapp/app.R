20a9ed330f68b6f0a20cb070dd20dbf4f70bf2a2
kgraylin <62913743+kgraylin@users.noreply.github.com>
2020-06-02 18:07:18 -0700

    Changed source file name

--- finalapp/app.R
+++ finalapp/app.R
@@ -1,12 +1,12 @@
-# App.R
+# app.R
 
 # Load libraries so they are available
 library("shiny")
 
-# Use source() to execute the `app_ui.R` and `app_server.R` files. These will
+# Use source() to execute the `ui.R` and `server.R` files. These will
 # define the UI value and server function respectively.
-source("app_ui.R")
-source("app_server.R")
+source("ui.R")
+source("server.R")
 
 # Create a new `shinyApp()` using the loaded `ui` and `server` variables
 shinyApp(ui = ui, server = server)
