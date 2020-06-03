819647ace7ade14db95b942532487c372c848fd3
Henry Bao <henrybao@uw.edu>
2020-06-03 15:16:43 -0400

    change file location for publishing

--- finalapp/server.R
+++ finalapp/server.R
@@ -7,15 +7,15 @@ library(plotly)
 library(ggplot2)
 
 # Load data frame
-df <- read.csv("../data/tourists-vs-gdp.csv",
+df <- read.csv("data/tourists-vs-gdp.csv",
                stringsAsFactors = FALSE)
 
 # Load scripts
-source("../scripts/summarytable.R")
-source("../scripts/summaryinfo.R")
-source("../scripts/chart-1.R")
-source("../scripts/chart-2.R")
-source("../scripts/chart-3.R")
+source("scripts/summarytable.R")
+source("scripts/summaryinfo.R")
+source("scripts/chart-1.R")
+source("scripts/chart-2.R")
+source("scripts/chart-3.R")
 
 # Define server function
 server <- function(input, output) {
